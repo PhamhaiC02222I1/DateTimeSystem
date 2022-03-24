297791bd73f721f4f98d660ce0e32e17a70d1e45
hai <humg.ola@gmail.com>
2022-03-24 20:51:24 +0700

    up

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
+
+
